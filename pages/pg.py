dff3860db70217966b4480adf9948cb5cd1786ab
syduc993 <106389091+syduc993@users.noreply.github.com>
2024-01-26 11:01:05 +0700

    update

--- pages/pg.py
+++ pages/pg.py
@@ -4,7 +4,8 @@ from dash import dcc, html
 dash.register_page(__name__, path='/', name='Procurement')
 
 layout = html.Iframe(
-    src='https://app.powerbi.com/view?r=eyJrIjoiMzAwN2E3YTEtNjdkZS00NWNkLWJkZDEtOTIyMzIzNDkwNzJlIiwidCI6IjkwYjY0ZWVlLTg0M2UtNDBiZi04ODZkLWZjMmMxNDk2NjE3NiIsImMiOjEwfQ%3D%3D',
+    #src='https://app.powerbi.com/view?r=eyJrIjoiMzAwN2E3YTEtNjdkZS00NWNkLWJkZDEtOTIyMzIzNDkwNzJlIiwidCI6IjkwYjY0ZWVlLTg0M2UtNDBiZi04ODZkLWZjMmMxNDk2NjE3NiIsImMiOjEwfQ%3D%3D',
+    src='https://app.powerbi.com/view?r=eyJrIjoiMTkzZjU2ZWYtYmNkMC00YmM2LTkzNTUtOTU5YjMxYjQ2ZjY2IiwidCI6IjkwYjY0ZWVlLTg0M2UtNDBiZi04ODZkLWZjMmMxNDk2NjE3NiIsImMiOjEwfQ%3D%3D'
     style={
         'height': '95vh',
         'width': '100%',
